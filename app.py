3107208ee08d218cdca656dabbfa4e2e9df34510
Padmaja Relangi Burdick <padmaja.relangiburdick@fsresidential.com>
2026-09-22 17:31:42 +0530

    updated input name

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.write("Hello P Databricks!")
 name = st.text_input("Enter your name")
 
 if name:
-   # st.success(f"Hello, {name}!")
-   st.success(f"Hello, ! Welcome to the Databricks app.")
+    st.success(f"Hello, {name}!")
+  
 
 st.divider()
 st.write("This app is running with Python and Streamlit.")
